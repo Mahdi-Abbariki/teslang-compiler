ba331f66ccb918d262f54167dbcf5a9eff20e9b4
Mahdi Abbariki <mahdi.abbariki.dev@gmail.com>
2021-11-07 22:03:54 +0330

    Lexical Analysis ## added another sample code file of tslang

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ include "autoloader.php";
 use Classes\Lexer;
 
 
-const TEST_FILE_ADDRESS = "dist/tslang.txt"; // you can change this line for different input file
+//const TEST_FILE_ADDRESS = "dist/tslang.txt"; // you can change this line for different input file
+const TEST_FILE_ADDRESS = "dist/test2.txt"; // you can change this line for different input file
+
 
 $lex = new Lexer(TEST_FILE_ADDRESS);
 
